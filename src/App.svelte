53972e6f137486b0aaacc77debd3a80583055060
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-03-29 17:12:46 +0100

    routing setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
     // importing Modules
+    import { Router, Route } from "svelte-navigator";
     import { _, locale } from "svelte-i18n";
+
+    // importing routes
+    import Home from "./routes/Home.svelte";
 
     // importing components
     import Component from "./lib/Component.svelte";
@@ -18,31 +22,36 @@
     };
 </script>
 
-<main class="h-screen bg-gray-300 dark:bg-gray-700">
-    <div class="text-gray-800 dark:text-gray-200">{$_("content")}</div>
-    <div class="flex flex-row gap-2">
-        <div
-            class="btn-primary btn"
-            on:click={() => {
-                changleLanguage("fr");
-            }}
-        >
-            french
+<Router>
+    <main class="h-screen bg-gray-300 dark:bg-gray-700">
+        <div class="text-gray-800 dark:text-gray-200">{$_("content")}</div>
+        <div class="flex flex-row gap-2">
+            <div
+                class="btn-primary btn"
+                on:click={() => {
+                    changleLanguage("fr");
+                }}
+            >
+                french
+            </div>
+            <div
+                class="btn-primary btn"
+                on:click={() => {
+                    changleLanguage("en");
+                }}
+            >
+                english
+            </div>
+            <div class="btn-secondary btn" on:click={toggleDarkMode}>
+                toggle dark mode
+            </div>
+            <Component />
         </div>
-        <div
-            class="btn-primary btn"
-            on:click={() => {
-                changleLanguage("en");
-            }}
-        >
-            english
-        </div>
-        <div class="btn-secondary btn" on:click={toggleDarkMode}>
-            toggle dark mode
-        </div>
-        <Component />
-    </div>
-</main>
+        <Route path="home">
+            <Home />
+        </Route>
+    </main>
+</Router>
 
 <style>
 </style>
